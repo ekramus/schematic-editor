096a99179036ea8d49c0b3be1e20641162d20b19
Urban Kravjansky <Urban Kravjansky@localhost>
2008-07-09 22:31:33 +0200

    -deploy.sh corrected

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ LIB=$DEPLOY/lib
 JAR=$DEPLOY/jar
 M2_REPO=~/.m2/repository
 
-rm -fR "$LIB"
-rm -fR "$JAR"
-rm -fR "$DEPLOY/*.log*"
-rm -fR "$DEPLOY/*.xml"
+rm -fR $LIB
+rm -fR $JAR
+rm -fR $DEPLOY/*.log*
+rm -fR $DEPLOY/*.xml
 
-mkdir "$JAR"
-mkdir "$LIB"
+mkdir $JAR
+mkdir $LIB
 
 cp "$M2_REPO/log4j/log4j/1.2.14/log4j-1.2.14.jar" "$LIB"
 cp "$M2_REPO/jgoodies/looks/1.2.2/looks-1.2.2.jar" "$LIB"
