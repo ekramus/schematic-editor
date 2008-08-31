a3fb281305d7f61fff1e36932dcc6b091135576e
Urban Kravjansky <ekramus@users.noreply.github.com>
2008-08-31 23:38:00 +0200

    -code partially prepared for Part implementation (part data in Part.java, shape in containded GroupNode

--- GraphNodes/src/main/java/cz/cvut/fel/schematicEditor/graphNode/WireNode.java
+++ GraphNodes/src/main/java/cz/cvut/fel/schematicEditor/graphNode/WireNode.java
@@ -1,29 +1,42 @@
 package cz.cvut.fel.schematicEditor.graphNode;
 
-import cz.cvut.fel.schematicEditor.element.element.Element;
+import cz.cvut.fel.schematicEditor.element.element.part.Wire;
 
 /**
  * This class represents Wire Node.
  * 
- * @author uk
+ * @author Urban Kravjansky
  */
 public class WireNode extends ElementNode {
+	/**
+	 * This is constructor.
+	 * 
+	 * @param wire
+	 *            <code>Wire</code> stored in this <code>WireNode</code>.
+	 */
+	public WireNode(Wire wire) {
+		super(wire);
+	}
 
-    /**
-     * @param element
-     */
-    public WireNode(Element element) {
-        super(element);
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * This is constructor.
+	 * 
+	 * @param wire
+	 *            <code>Wire</code> stored in this <code>WireNode</code>.
+	 * @param id
+	 *            identifier of this <code>WireNode</code>.
+	 */
+	public WireNode(Wire wire, String id) {
+		super(wire, id);
+	}
 
-    /**
-     * @see cz.cvut.fel.schematicEditor.graphNode.Node#duplicate()
-     */
-    @Override
-    public Node duplicate() {
-        WireNode result = new WireNode(getElement());
+	/**
+	 * @see cz.cvut.fel.schematicEditor.graphNode.Node#duplicate()
+	 */
+	@Override
+	public Node duplicate() {
+		WireNode result = new WireNode((Wire) getElement());
 
-        return result;
-    }
+		return result;
+	}
 }
