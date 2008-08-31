1d7c174233e728d482974092b78d6dedcf0bd664
Urban Kravjansky <Urban Kravjansky@localhost>
2008-08-31 23:38:00 +0200

    -code partially prepared for Part implementation (part data in Part.java, shape in containded GroupNode

--- GraphNodes/src/main/java/cz/cvut/fel/schematicEditor/graphNode/PartNode.java
+++ GraphNodes/src/main/java/cz/cvut/fel/schematicEditor/graphNode/PartNode.java
@@ -1,29 +1,66 @@
 package cz.cvut.fel.schematicEditor.graphNode;
 
-import cz.cvut.fel.schematicEditor.element.element.Element;
+import cz.cvut.fel.schematicEditor.element.element.part.Part;
 
 /**
- * This class represents Part Node.
+ * This class represents Part Node. It contains link to {@link Part} and to
+ * {@link GroupNode} with part's visual form.
  * 
- * @author uk
+ * @author Urban Kravjansky
  */
 public class PartNode extends ElementNode {
+	/**
+	 * GroupNode containing graphic representation of part shape.
+	 */
+	private GroupNode partGroupNode;
 
-    /**
-     * @param element
-     */
-    public PartNode(Element element) {
-        super(element);
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * This is constructor.
+	 * 
+	 * @param part
+	 *            <code>Part</code> stored in this <code>PartNode</code>.
+	 */
+	public PartNode(Part part, GroupNode partGroupNode) {
+		super(part);
+		setPartGroupNode(partGroupNode);
+	}
 
-    /**
-     * @see cz.cvut.fel.schematicEditor.graphNode.Node#duplicate()
-     */
-    @Override
-    public Node duplicate() {
-        PartNode result = new PartNode(getElement());
+	/**
+	 * This is constructor.
+	 * 
+	 * @param part
+	 *            <code>Part</code> stored in this <code>PartNode</code>.
+	 * @param id
+	 *            identifier of this <code>PartNode</code>.
+	 */
+	public PartNode(Part part, GroupNode partGroupNode, String id) {
+		super(part, id);
+		setPartGroupNode(partGroupNode);
+	}
 
-        return result;
-    }
+	/**
+	 * @see cz.cvut.fel.schematicEditor.graphNode.Node#duplicate()
+	 */
+	@Override
+	public Node duplicate() {
+		PartNode result = new PartNode((Part) getElement(),
+				(GroupNode) getPartGroupNode().duplicate());
+
+		return result;
+	}
+
+	/**
+	 * @param partGroupNode
+	 *            the partGroupNode to set
+	 */
+	private void setPartGroupNode(GroupNode partGroupNode) {
+		this.partGroupNode = partGroupNode;
+	}
+
+	/**
+	 * @return the partGroupNode
+	 */
+	public GroupNode getPartGroupNode() {
+		return partGroupNode;
+	}
 }
