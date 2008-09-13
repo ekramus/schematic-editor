256b77aa76c6378cf0a9a96e80783e856ab27fcb
Urban Kravjansky <Urban Kravjansky@localhost>
2008-09-13 23:50:53 +0200

    -commit before part creation will be implemented

--- GraphNodes/src/main/java/cz/cvut/fel/schematicEditor/graphNode/ShapeNode.java
+++ GraphNodes/src/main/java/cz/cvut/fel/schematicEditor/graphNode/ShapeNode.java
@@ -4,13 +4,13 @@ import cz.cvut.fel.schematicEditor.element.element.shape.Shape;
 
 /**
  * This class represents Shape Node.
- * 
+ *
  * @author uk
  */
 public class ShapeNode extends ElementNode {
     /**
      * This is constructor.
-     * 
+     *
      * @param shape <code>Shape</code> stored in this <code>ShapeNode</code>.
      */
     public ShapeNode(Shape shape) {
@@ -19,7 +19,7 @@ public class ShapeNode extends ElementNode {
 
     /**
      * This is constructor.
-     * 
+     *
      * @param shape <code>Shape</code> stored in this <code>ShapeNode</code>.
      * @param id identifier of this <code>ShapeNode</code>.
      */
