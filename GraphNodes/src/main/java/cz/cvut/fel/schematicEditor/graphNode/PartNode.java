6b4389243a73eab398e73f999c3c283259ef975b
Urban Kravjansky <ekramus@users.noreply.github.com>
2008-09-13 22:32:03 +0200

    -part selection and manipulation working

--- GraphNodes/src/main/java/cz/cvut/fel/schematicEditor/graphNode/PartNode.java
+++ GraphNodes/src/main/java/cz/cvut/fel/schematicEditor/graphNode/PartNode.java
@@ -1,6 +1,7 @@
 package cz.cvut.fel.schematicEditor.graphNode;
 
 import java.awt.Color;
+import java.awt.geom.Rectangle2D.Double;
 
 import cz.cvut.fel.schematicEditor.element.element.part.Part;
 import cz.cvut.fel.schematicEditor.unit.oneDimensional.Unit;
@@ -9,121 +10,137 @@ import cz.cvut.fel.schematicEditor.unit.twoDimesional.UnitRectangle;
 /**
  * This class represents Part Node. It contains link to {@link Part} and to
  * {@link GroupNode} with part's visual form.
- * 
+ *
  * @author Urban Kravjansky
  */
 public class PartNode extends ElementNode {
-	/**
-	 * GroupNode containing graphic representation of part shape.
-	 */
-	private GroupNode partGroupNode;
-	/**
-	 * {@link ParameterNode} containing parameters of part shape.
-	 */
-	private ParameterNode partParameterNode;
+    /**
+     * GroupNode containing graphic representation of part shape.
+     */
+    private GroupNode partGroupNode;
+    /**
+     * {@link ParameterNode} containing parameters of part shape.
+     */
+    private ParameterNode partParameterNode;
 
-	/**
-	 * This is constructor.
-	 * 
-	 * @param part
-	 *            <code>Part</code> stored in this <code>PartNode</code>.
-	 */
-	public PartNode(Part part, GroupNode partGroupNode) {
-		super(part);
+    /**
+     * This is constructor.
+     *
+     * @param part
+     *            <code>Part</code> stored in this <code>PartNode</code>.
+     */
+    public PartNode(Part part, GroupNode partGroupNode) {
+        super(part);
 
-		initialize(partGroupNode);
-	}
+        initialize(partGroupNode);
+    }
 
-	/**
-	 * This is constructor.
-	 * 
-	 * @param part
-	 *            <code>Part</code> stored in this <code>PartNode</code>.
-	 * @param id
-	 *            identifier of this <code>PartNode</code>.
-	 */
-	public PartNode(Part part, GroupNode partGroupNode, String id) {
-		super(part, id);
+    /**
+     * This is constructor.
+     *
+     * @param part
+     *            <code>Part</code> stored in this <code>PartNode</code>.
+     * @param id
+     *            identifier of this <code>PartNode</code>.
+     */
+    public PartNode(Part part, GroupNode partGroupNode, String id) {
+        super(part, id);
 
-		initialize(partGroupNode);
-	}
+        initialize(partGroupNode);
+    }
 
-	/**
-	 * Initializes all necessary variables.
-	 * 
-	 * @param partGroupNode
-	 *            {@link GroupNode} describing shape of {@link Part}.
-	 */
-	private void initialize(GroupNode partGroupNode) {
-		ParameterNode pn = partGroupNode.getChildrenParameterNode();
-		// set parameter node if not set
-		if (pn == null) {
-			pn = new ParameterNode();
-			pn.setColor(Color.RED);
-			partGroupNode.add(pn);
-		}
+    /**
+     * Initializes all necessary variables.
+     *
+     * @param partGroupNode
+     *            {@link GroupNode} describing shape of {@link Part}.
+     */
+    private void initialize(GroupNode partGroupNode) {
+        ParameterNode pn = partGroupNode.getChildrenParameterNode();
+        // set parameter node if not set
+        if (pn == null) {
+            pn = new ParameterNode();
+            pn.setColor(Color.RED);
+            partGroupNode.add(pn);
+        }
 
-		setPartGroupNode(partGroupNode);
-	}
+        setPartGroupNode(partGroupNode);
+    }
 
-	/**
-	 * Getter for element bounds.
-	 * 
-	 * @param boundModifier
-	 *            modifier which affects boundary size of element.
-	 * @return Bounds of element.
-	 */
-	public UnitRectangle getBounds(Unit boundModifier) {
-		double x = getPartGroupNode().getBounds().getX()
-				- boundModifier.doubleValue();
-		double y = getPartGroupNode().getBounds().getY()
-				- boundModifier.doubleValue();
-		double w = getPartGroupNode().getBounds().getWidth() + 2
-				* boundModifier.doubleValue();
-		double h = getPartGroupNode().getBounds().getHeight() + 2
-				* boundModifier.doubleValue();
+    /**
+     * @see cz.cvut.fel.schematicEditor.graphNode.ElementNode#isHit(java.awt.geom.Rectangle2D.Double)
+     */
+    @Override
+    protected boolean isHit(Double point) {
+        return getPartGroupNode().isHit(point);
+    }
 
-		return new UnitRectangle(x, y, w, h);
-	}
+    /**
+     * Getter for element bounds.
+     *
+     * @param boundModifier
+     *            modifier which affects boundary size of element.
+     * @return Bounds of element.
+     */
+    public UnitRectangle getBounds(Unit boundModifier) {
+        double x = getPartGroupNode().getBounds().getX()
+                - boundModifier.doubleValue();
+        double y = getPartGroupNode().getBounds().getY()
+                - boundModifier.doubleValue();
+        double w = getPartGroupNode().getBounds().getWidth() + 2
+                * boundModifier.doubleValue();
+        double h = getPartGroupNode().getBounds().getHeight() + 2
+                * boundModifier.doubleValue();
 
-	/**
-	 * @see cz.cvut.fel.schematicEditor.graphNode.Node#duplicate()
-	 */
-	@Override
-	public Node duplicate() {
-		PartNode result = new PartNode((Part) getElement(),
-				(GroupNode) getPartGroupNode().duplicate());
+        return new UnitRectangle(x, y, w, h);
+    }
 
-		return result;
-	}
+    /**
+     * @see cz.cvut.fel.schematicEditor.graphNode.Node#duplicate()
+     */
+    @Override
+    public Node duplicate() {
+        PartNode result = new PartNode((Part) getElement(),
+                (GroupNode) getPartGroupNode().duplicate());
 
-	/**
-	 * @param partGroupNode
-	 *            the partGroupNode to set
-	 */
-	private void setPartGroupNode(GroupNode partGroupNode) {
-		this.partGroupNode = partGroupNode;
-	}
+        return result;
+    }
 
-	/**
-	 * @return the partGroupNode
-	 */
-	public GroupNode getPartGroupNode() {
-		return this.partGroupNode;
-	}
+    /**
+     * @param partGroupNode
+     *            the partGroupNode to set
+     */
+    private void setPartGroupNode(GroupNode partGroupNode) {
+        this.partGroupNode = partGroupNode;
+    }
 
-	/**
-	 * @param partParameterNode
-	 *            the partParameterNode to set
-	 */
-	private void setPartParameterNode(ParameterNode partParameterNode) {
-		this.partParameterNode = partParameterNode;
-	}
+    /**
+     * @return the partGroupNode
+     */
+    public GroupNode getPartGroupNode() {
+        return this.partGroupNode;
+    }
 
-	/**
-	 * @return the partParameterNode
-	 */
-	public ParameterNode getPartParameterNode() {
-		return this.partParameterNode;
-	}
+    /**
+     * @param partParameterNode
+     *            the partParameterNode to set
+     */
+    private void setPartParameterNode(ParameterNode partParameterNode) {
+        this.partParameterNode = partParameterNode;
+    }
+
+    /**
+     * @return the partParameterNode
+     */
+    public ParameterNode getPartParameterNode() {
+        return this.partParameterNode;
+    }
+
+    /**
+     * @see cz.cvut.fel.schematicEditor.graphNode.ElementNode#toString()
+     */
+    @Override
+    public String toString() {
+        return "[PartNode]";
+    }
 }
