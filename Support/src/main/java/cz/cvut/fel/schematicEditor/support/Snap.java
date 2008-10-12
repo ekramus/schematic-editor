4e1134700d6b2777b2cf0ec9e7b139e5b9315c1d
Urban Kravjansky <Urban Kravjansky@localhost>
2008-10-12 11:09:17 +0200

    -Snap modified to use UnitPoint, prepared for advanced snap

--- Support/src/main/java/cz/cvut/fel/schematicEditor/support/Snap.java
+++ Support/src/main/java/cz/cvut/fel/schematicEditor/support/Snap.java
@@ -1,31 +1,58 @@
 package cz.cvut.fel.schematicEditor.support;
 
+import java.util.Vector;
+
 import cz.cvut.fel.schematicEditor.configuration.GuiConfiguration;
 import cz.cvut.fel.schematicEditor.unit.oneDimensional.computer.Pixel;
+import cz.cvut.fel.schematicEditor.unit.twoDimesional.UnitPoint;
 
 /**
  * This class implements <em>snap to grid</em> feature. It is dependent on <code>gridSize</code> and <code>snap</code>
  * attributes.
- * 
+ *
  * @author Urban Kravjansky
  */
 public class Snap {
     /**
      * Get snapped coordinate. In case snap to grid is disabled, coordinate will be unchanged.
-     * 
+     *
      * @param coordinate coordinate to snap.
      * @return Snapped coordinate value.
      */
-    public static Pixel getSnap(double coordinate) {
+    public static UnitPoint getSnap(UnitPoint coordinate) {
+        GuiConfiguration configuration = GuiConfiguration.getInstance();
+        Pixel x;
+        Pixel y;
+
+        if (!configuration.isSnapToGrid()) {
+            return new UnitPoint(coordinate);
+        }
+        x = new Pixel(configuration.getGridSize().doubleValue() * (int) ((coordinate.getX() + configuration
+                .getGridSize().doubleValue() / 2) / configuration.getGridSize().doubleValue()));
+        y = new Pixel(configuration.getGridSize().doubleValue() * (int) ((coordinate.getY() + configuration
+                .getGridSize().doubleValue() / 2) / configuration.getGridSize().doubleValue()));
+
+        return new UnitPoint(x, y);
+    }
+
+    /**
+     * Get snapped coordinate from the list of coordinates. In case no coordinate is inside delta circle, returns
+     * original coordinate.
+     *
+     * @param coordinate coordinate to snap.
+     * @param snapPoints {@link Vector} of {@link UnitPoint}s, from which snap point is selected.
+     * @return Snapped coordinate value.
+     */
+    public static UnitPoint getSnap(UnitPoint coordinate, Vector<UnitPoint> snapPoints) {
         GuiConfiguration configuration = GuiConfiguration.getInstance();
         Pixel result;
 
         if (!configuration.isSnapToGrid()) {
-            return new Pixel(coordinate);
+            return new UnitPoint(coordinate);
         }
-        result = new Pixel(configuration.getGridSize().doubleValue() * (int) ((coordinate + configuration.getGridSize()
-                .doubleValue() / 2) / configuration.getGridSize().doubleValue()));
+        result = new Pixel(configuration.getGridSize().doubleValue() * (int) ((coordinate.getX() + configuration
+                .getGridSize().doubleValue() / 2) / configuration.getGridSize().doubleValue()));
 
-        return result;
+        return null;
     }
 }
--- Support/src/test/java/cz/cvut/fel/schematicEditor/support/SnapTest.java
+++ Support/src/test/java/cz/cvut/fel/schematicEditor/support/SnapTest.java
@@ -1,20 +1,21 @@
 package cz.cvut.fel.schematicEditor.support;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 import cz.cvut.fel.schematicEditor.configuration.GuiConfiguration;
 import cz.cvut.fel.schematicEditor.unit.oneDimensional.computer.Pixel;
+import cz.cvut.fel.schematicEditor.unit.twoDimesional.UnitPoint;
 
 /**
  * JUnit test class for {@link Snap}.
- * 
+ *
  * @author Urban Kravjansky
  */
 public class SnapTest {
     /**
-     * Test method for {@link Snap#getSnap(double)} method.
+     * Test method for {@link Snap#getSnap(UnitPoint)} method.
      */
     @Test
     public void getSnap() {
@@ -27,7 +28,8 @@ public class SnapTest {
         // from 0 to 9, no snap
         configuration.setSnapToGrid(false);
         while (d < 2 * gridSize) {
-            assertEquals(Snap.getSnap(d), new Pixel(d));
+            UnitPoint up = new UnitPoint(d, d);
+            assertEquals(Snap.getSnap(up), new UnitPoint(d, d));
             d++;
         }
 
@@ -35,7 +37,8 @@ public class SnapTest {
         configuration.setSnapToGrid(true);
         d = 0;
         while (d < 2 * gridSize) {
-            assertEquals((int) (Snap.getSnap(d).doubleValue() / gridSize), (int) ((d + gridSize / 2) / gridSize));
+            UnitPoint up = new UnitPoint(d, d);
+            assertEquals((int) (Snap.getSnap(up).getX() / gridSize), (int) ((d + gridSize / 2) / gridSize));
             d++;
         }
     }
