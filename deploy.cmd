bf171cb463716b5663d9d78e092378373966f482
Urban Kravjansky <Urban Kravjansky@localhost>
2008-11-17 22:42:59 +0100

    -deploy.cmd script modified to work with plugins

--- deploy.cmd
+++ deploy.cmd
@@ -4,10 +4,12 @@ set DEPLOY=deploy
 set LIB=%DEPLOY%\lib
 set JAR=%DEPLOY%\jar
 set CONF=%DEPLOY%\config
+set PLUGIN=%DEPLOY%\plugins
 set M2_REPO="%USERPROFILE%\.m2\repository"
 
 del /Q %LIB%
 del /Q %JAR%
+del /Q %PLUGIN%
 del /Q %DEPLOY%\*.log
 del /Q %DEPLOY%\*.log.*
 del /Q %DEPLOY%\*.xml
@@ -15,6 +17,7 @@ del /Q %DEPLOY%\*.xml
 mkdir %JAR%
 mkdir %LIB%
 mkdir %CONF%
+mkdir %PLUGIN%
 
 copy %M2_REPO%\log4j\log4j\1.2.14\log4j-1.2.14.jar %LIB%
 copy %M2_REPO%\jgoodies\looks\1.2.2\looks-1.2.2.jar %LIB%
@@ -31,3 +34,6 @@ copy Configuration\target\Configuration-1.0-SNAPSHOT.jar %JAR%
 copy Units\target\Units-1.0-SNAPSHOT.jar %JAR%
 
 copy Launchers\log4j.xml %DEPLOY%
+
+rem copy plugins
+copy Plugins-ElementsCount\target\Plugins-ElementsCount-1.0-SNAPSHOT.jar %PLUGIN% 
