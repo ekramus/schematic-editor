e24cfbcabbcac20a4faa65997d0536b36ee3db36
Urban Kravjansky <Urban Kravjansky@localhost>
2008-11-21 18:40:07 +0100

    -rotate now able to do +-90 degrees rotation

--- Gui/src/main/java/cz/cvut/fel/schematicEditor/application/guiElements/menuBar/listeners/RotateElementMenuItemListener.java
+++ Gui/src/main/java/cz/cvut/fel/schematicEditor/application/guiElements/menuBar/listeners/RotateElementMenuItemListener.java
@@ -10,6 +10,7 @@ import cz.cvut.fel.schematicEditor.manipulation.Manipulation;
 import cz.cvut.fel.schematicEditor.manipulation.ManipulationFactory;
 import cz.cvut.fel.schematicEditor.manipulation.ManipulationType;
 import cz.cvut.fel.schematicEditor.manipulation.exception.UnknownManipulationException;
+import cz.cvut.fel.schematicEditor.unit.twoDimesional.UnitPoint;
 
 /**
  * This class implements {@link ActionListener} for <code>viewPartPropertiesMenuItem</code> in {@link MenuBar}.
@@ -17,6 +18,20 @@ import cz.cvut.fel.schematicEditor.manipulation.exception.UnknownManipulationExc
  * @author Urban Kravjansky
  */
 public class RotateElementMenuItemListener implements ActionListener {
+    /**
+     * Rotation orientation.
+     */
+    private UnitPoint orientation;
+
+    /**
+     * This method instantiates new instance.
+     *
+     */
+    public RotateElementMenuItemListener(UnitPoint orientation) {
+        super();
+
+        setOrientation(orientation);
+    }
 
     /**
      * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
@@ -28,11 +43,26 @@ public class RotateElementMenuItemListener implements ActionListener {
                     Manipulation m = ManipulationFactory.create(ManipulationType.ROTATE, ScenePanel.getInstance()
                             .getSchemeSG().getTopNode());
                     m.setManipulatedGroup(Structures.getActiveManipulation().getManipulatedGroup());
+                    m.addManipulationCoordinates(getOrientation().getUnitX(), getOrientation().getUnitY());
                     Structures.getManipulationQueue().execute(m);
                 } catch (UnknownManipulationException ume) {
                     ume.printStackTrace();
                 }
             }
         }
+    }
+
+    /**
+     * @param orientation the orientation to set
+     */
+    private void setOrientation(UnitPoint orientation) {
+        this.orientation = orientation;
+    }
+
+    /**
+     * @return the orientation
+     */
+    private UnitPoint getOrientation() {
+        return this.orientation;
     }
 }
