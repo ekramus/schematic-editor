4c75abedab67a15ddcc85d97f904c27126ffbda0
Urban Kravjansky <Urban Kravjansky@localhost>
2008-12-22 15:03:39 +0100

    -selected part is now correctly disassembled into elements for editing

--- GuiAdvanced/src/main/java/cz/cvut/fel/schematicEditor/guiAdvanced/guiElements/menuBar/listeners/EditPartMenuItemListener.java
+++ GuiAdvanced/src/main/java/cz/cvut/fel/schematicEditor/guiAdvanced/guiElements/menuBar/listeners/EditPartMenuItemListener.java
@@ -4,8 +4,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import cz.cvut.fel.schematicEditor.element.ElementType;
+import cz.cvut.fel.schematicEditor.graphNode.ConnectorNode;
 import cz.cvut.fel.schematicEditor.graphNode.GroupNode;
 import cz.cvut.fel.schematicEditor.graphNode.ParameterNode;
+import cz.cvut.fel.schematicEditor.graphNode.PartNode;
 import cz.cvut.fel.schematicEditor.guiAdvanced.guiElements.guiAdvanced.GuiAdvanced;
 import cz.cvut.fel.schematicEditor.guiAdvanced.guiElements.menuBar.MenuBar;
 import cz.cvut.fel.schematicEditor.manipulation.ManipulationType;
@@ -34,9 +36,6 @@ public final class EditPartMenuItemListener implements ActionListener {
      *            needed.
      */
     public void actionPerformed(final ActionEvent ae) {
-        MenuBar.getInstance().setEditPartMenuItemEnabled(false);
-        MenuBar.getInstance().setDoneEditPartMenuItemEnabled(true);
-
         Select s;
         if (GuiAdvanced.getInstance().getSceneScenePanel().getActiveManipulation().getManipulationType() == ManipulationType.SELECT) {
             s = (Select) GuiAdvanced.getInstance().getSceneScenePanel().getActiveManipulation();
@@ -45,14 +44,27 @@ public final class EditPartMenuItemListener implements ActionListener {
         }
 
         if (s.getManipulatedGroup().getChildrenElementList().getFirst().getElement().getElementType() == ElementType.T_PART) {
-            GroupNode gn = new GroupNode();
-            gn.add(new ParameterNode());
-            gn.add(s.getManipulatedGroup());
-            GuiAdvanced.getInstance().getPartScenePanel().getSceneGraph().initSceneGraph(gn);
+            GroupNode topNode = new GroupNode();
+            topNode.add(new ParameterNode());
 
             // TODO create GroupNode from partGroupNode
+            PartNode pn = (PartNode) s.getManipulatedGroup().getChildrenElementList().getFirst();
+            GroupNode partGroupNode = (GroupNode) pn.getPartGroupNode().duplicate();
+
+            for (ConnectorNode connectorNode : pn.getPartConnectors()) {
+                GroupNode gn = new GroupNode();
+                gn.add(connectorNode.duplicate());
+                gn.add(new ParameterNode());
+                partGroupNode.add(gn);
+            }
             // TODO edit this GroupNode
+            topNode.add(partGroupNode);
+            GuiAdvanced.getInstance().getPartScenePanel().getSceneGraph().initSceneGraph(topNode);
             // TODO after done edit part, fill partGroupNode with modified GroupNode
+
+            // set correct state of menu items
+            MenuBar.getInstance().setEditPartMenuItemEnabled(false);
+            MenuBar.getInstance().setDoneEditPartMenuItemEnabled(true);
         }
     }
 }
