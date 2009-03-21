ce35c17ab86660900175ed964ce4b8efb82e90aa
Urban Kravjansky <ekramus@users.noreply.github.com>
2009-03-22 00:01:23 +0100

    -part browser now displays part icons

---
+++ Gui/src/main/java/cz/cvut/fel/schematicEditor/guiAdvanced/guiElements/partBrowser/PartBrowserNodeRenderer.java
@@ -0,0 +1,33 @@
+package cz.cvut.fel.schematicEditor.guiAdvanced.guiElements.partBrowser;
+
+import java.awt.Component;
+
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
+
+/**
+ * @author Urban Kravjansky
+ *
+ */
+public class PartBrowserNodeRenderer extends DefaultTreeCellRenderer {
+    /**
+     * @see javax.swing.tree.DefaultTreeCellRenderer#getTreeCellRendererComponent(javax.swing.JTree, java.lang.Object,
+     *      boolean, boolean, boolean, int, boolean)
+     */
+    @Override
+    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded,
+            boolean leaf, int row, boolean hasFocus) {
+        super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
+
+        if (leaf) {
+            DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
+            PartBrowserNode pbn = (PartBrowserNode) node.getUserObject();
+
+            setIcon(pbn.getIcon());
+            setText(pbn.getVariant() + " (" + pbn.getDescription() + ")");
+        }
+
+        return this;
+    }
+}
