854a31f47e39063d261e95e4249728c2af378c3c
Urban Kravjansky <Urban Kravjansky@localhost>
2009-05-02 18:40:05 +0200

    -RemoteConfiguration module added

---
+++ RemoteConfiguration/src/main/java/cz/cvut/fel/schematicEditor/remoteConfiguration/RemoteConfiguration.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.schematicEditor.remoteConfiguration;
+
+/**
+ *
+ * @author Urban Kravjansky
+ */
+public class RemoteConfiguration {
+    /**
+     */
+    public RemoteConfiguration() {
+    }
+
+}
