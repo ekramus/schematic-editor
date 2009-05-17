88fa477cece6b6c5242d68c1a2db2bb5fcd285ae
Urban Kravjansky <Urban Kravjansky@localhost>
2009-05-17 20:54:33 +0200

    -SynchronizedPartProperties implementation started

---
+++ Parts/src/main/java/cz/cvut/fel/schematicEditor/parts/SynchronizedPartProperty.java
@@ -0,0 +1,213 @@
+package cz.cvut.fel.schematicEditor.parts;
+
+import java.util.HashMap;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Urban Kravjansky
+ *
+ */
+public class SynchronizedPartProperty {
+    /**
+     * {@link Logger} instance for logging purposes.
+     */
+    private static Logger logger;
+    /**
+     * The definition.
+     */
+    private String        definition;
+    /**
+     * The name cs.
+     */
+    private String        nameCS;
+    /**
+     * The name en.
+     */
+    private String        nameEN;
+    /**
+     * The type.
+     */
+    private String        type;
+    /**
+     * The help cs.
+     */
+    private String        helpCS;
+    /**
+     * The help en.
+     */
+    private String        helpEN;
+    /**
+     * The validate.
+     */
+    private String        validate;
+    /**
+     * The use.
+     */
+    private String        use;
+    /**
+     * The show.
+     */
+    private String        show;
+
+    /**
+     * This method instantiates new instance.
+     *
+     * @param sppMap {@link HashMap} used for initialization of this instance.
+     *
+     */
+    public SynchronizedPartProperty(HashMap<String, String> sppMap) {
+        logger = Logger.getLogger(getClass());
+
+        for (String key : sppMap.keySet()) {
+            if (key.matches(SynchronizedPartPropertyEnum.DEFINITION.getValue())) {
+                setDefinition(sppMap.get(key));
+            } else if (key.matches(SynchronizedPartPropertyEnum.HELP_CS.getValue())) {
+                setHelpCS(sppMap.get(key));
+            } else if (key.matches(SynchronizedPartPropertyEnum.HELP_EN.getValue())) {
+                setHelpEN(sppMap.get(key));
+            } else if (key.matches(SynchronizedPartPropertyEnum.TYPE.getValue())) {
+                setType(sppMap.get(key));
+            } else if (key.matches(SynchronizedPartPropertyEnum.NAME_CS.getValue())) {
+                setNameCS(sppMap.get(key));
+            } else if (key.matches(SynchronizedPartPropertyEnum.NAME_EN.getValue())) {
+                setNameEN(sppMap.get(key));
+            } else if (key.matches(SynchronizedPartPropertyEnum.VALIDATE.getValue())) {
+                setValidate(sppMap.get(key));
+            } else if (key.matches(SynchronizedPartPropertyEnum.USE.getValue())) {
+                setUse(sppMap.get(key));
+            } else if (key.matches(SynchronizedPartPropertyEnum.SHOW.getValue())) {
+                setShow(sppMap.get(key));
+            } else {
+                logger.error("Unknow property field");
+            }
+        }
+    }
+
+    /**
+     * @return the show
+     */
+    public String getShow() {
+        return this.show;
+    }
+
+    /**
+     * @param show the show to set
+     */
+    private void setShow(String show) {
+        this.show = show;
+    }
+
+    /**
+     * @return the definition
+     */
+    public String getDefinition() {
+        return this.definition;
+    }
+
+    /**
+     * @param definition the definition to set
+     */
+    private void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    /**
+     * @return the nameCS
+     */
+    public String getNameCS() {
+        return this.nameCS;
+    }
+
+    /**
+     * @param nameCS the nameCS to set
+     */
+    private void setNameCS(String nameCS) {
+        this.nameCS = nameCS;
+    }
+
+    /**
+     * @return the nameEN
+     */
+    public String getNameEN() {
+        return this.nameEN;
+    }
+
+    /**
+     * @param nameEN the nameEN to set
+     */
+    private void setNameEN(String nameEN) {
+        this.nameEN = nameEN;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return this.type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    private void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the helpCS
+     */
+    public String getHelpCS() {
+        return this.helpCS;
+    }
+
+    /**
+     * @param helpCS the helpCS to set
+     */
+    private void setHelpCS(String helpCS) {
+        this.helpCS = helpCS;
+    }
+
+    /**
+     * @return the helpEN
+     */
+    public String getHelpEN() {
+        return this.helpEN;
+    }
+
+    /**
+     * @param helpEN the helpEN to set
+     */
+    private void setHelpEN(String helpEN) {
+        this.helpEN = helpEN;
+    }
+
+    /**
+     * @return the validate
+     */
+    public String getValidate() {
+        return this.validate;
+    }
+
+    /**
+     * @param validate the validate to set
+     */
+    private void setValidate(String validate) {
+        this.validate = validate;
+    }
+
+    /**
+     * @return the use
+     */
+    public String getUse() {
+        return this.use;
+    }
+
+    /**
+     * @param use the use to set
+     */
+    private void setUse(String use) {
+        this.use = use;
+    }
+
+}
---
+++ Parts/src/main/java/cz/cvut/fel/schematicEditor/parts/SynchronizedPartPropertyEnum.java
@@ -0,0 +1,73 @@
+package cz.cvut.fel.schematicEditor.parts;
+
+/**
+ * @author Urban Kravjansky
+ *
+ */
+public enum SynchronizedPartPropertyEnum {
+    /**
+     * Definition of property.
+     */
+    DEFINITION("\\[.*\\]"),
+    /**
+     * Czech name of property.
+     */
+    NAME_CS("name_cs"),
+    /**
+     * English name of property.
+     */
+    NAME_EN("name_en"),
+    /**
+     * Type of property field.
+     */
+    TYPE("type"),
+    /**
+     * Method of validation.
+     */
+    VALIDATE("validate"),
+    /**
+     * Czech help.
+     */
+    HELP_CS("help_cs"),
+    /**
+     * English help.
+     */
+    HELP_EN("help_en"),
+    /**
+     * Type of usage.
+     */
+    USE("use"),
+    /**
+     * Indicates, whether property is shown or not.
+     */
+    SHOW("show");
+
+    /**
+     * Regexp {@link String} value of specified enum.
+     */
+    private String value;
+
+    /**
+     * This method instantiates new instance.
+     *
+     * @param value value of property, which is added.
+     *
+     */
+    private SynchronizedPartPropertyEnum(String value) {
+        setValue(value);
+    }
+
+    /**
+     * @return the value
+     */
+    public String getValue() {
+        return this.value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    private void setValue(String value) {
+        this.value = value;
+    }
+}
