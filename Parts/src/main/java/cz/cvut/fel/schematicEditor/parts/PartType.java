ebcb256dbebf5be1566ee8ff598e289573195ae3
Urban Kravjansky <Urban Kravjansky@localhost>
2009-08-11 21:48:59 +0200

    -corrected part models

--- Parts/src/main/java/cz/cvut/fel/schematicEditor/parts/PartType.java
+++ Parts/src/main/java/cz/cvut/fel/schematicEditor/parts/PartType.java
@@ -9,21 +9,65 @@ public enum PartType {
     /**
      * Identifier for resistor part.
      */
-    RESISTOR,
+    RESISTOR("R", "rezistor"),
     /**
      * Identifier for inductor part.
      */
-    INDUCTOR,
+    INDUCTOR("L", "induktor"),
     /**
      * Identifier for capacitor part.
      */
-    CAPACITOR,
+    CAPACITOR("C", "capacitor"),
     /**
      * Identifier for voltage source part.
      */
-    VOLTAGE_SOURCE,
+    VOLTAGE_SOURCE("U", "voltage source"),
     /**
      * Identifier for current source part.
      */
-    CURRENT_SOURCE,
+    CURRENT_SOURCE("I", "current source"),
+    /**
+     * Identifier for transformator part.
+     */
+    TRANSFORMATOR("T", "transformator");
+
+    private String variant;
+    private String description;
+
+    /**
+     * @return the variant
+     */
+    public String getVariant() {
+        return this.variant;
+    }
+
+    /**
+     * @param variant the variant to set
+     */
+    private void setVariant(String variant) {
+        this.variant = variant;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return this.description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    private void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * This method instantiates new instance.
+     *
+     */
+    private PartType(String variant, String description) {
+        setVariant(variant);
+        setDescription(description);
+    }
 }
