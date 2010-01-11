a2fd95fb608ea8159be99ae1d1d13d8e4a163500
Karel <Karel@KAJINEK>
2010-01-11 12:11:43 +0100

    changed making of Junction. To do: - r2d has to use the center

--- Gui/src/main/java/cz/cvut/fel/schematicEditor/guiAdvanced/guiElements/scenePanel/listeners/ScenePanelMouseMotionListener.java
+++ Gui/src/main/java/cz/cvut/fel/schematicEditor/guiAdvanced/guiElements/scenePanel/listeners/ScenePanelMouseMotionListener.java
@@ -72,35 +72,26 @@ public class ScenePanelMouseMotionListener implements MouseMotionListener {
 		Gui.getActiveScenePanel().setActualPointerCoordinates(sb);
 		// adjust coordinate according to relative start
 		logger.trace("SB: " + sb);
-		sb
-				.setX(sb.getX()
-						- Gui.getActiveScenePanel().getRelativeStart().getX());
-		sb
-				.setY(sb.getY()
-						- Gui.getActiveScenePanel().getRelativeStart().getY());
+		sb.setX(sb.getX() - Gui.getActiveScenePanel().getRelativeStart().getX());
+		sb.setY(sb.getY() - Gui.getActiveScenePanel().getRelativeStart().getY());
 		// set label
 		StatusBar.getInstance().setCoordinatesJLabel(
-				"X: " + sb.getUnitX() + " Y: " + sb.getUnitY() + " "
-						+ Gui.getActiveScenePanel().getRelativeStart());
+				"X: " + sb.getUnitX() + " Y: " + sb.getUnitY() + " " + Gui.getActiveScenePanel().getRelativeStart());
 
 		try {
 			Manipulation m = Gui.getActiveScenePanel().getActiveManipulation();
 
 			// manipulation is active
 			if (m.isActive()) {
-				UnitPoint up = m.getScaledUnitPoint(e.getX(), e.getY(), Gui
-						.getActiveScenePanel().getZoomFactor());
+				UnitPoint up = m.getScaledUnitPoint(e.getX(), e.getY(), Gui.getActiveScenePanel().getZoomFactor());
 				UnitPoint snap;
 				if (m.getManipulationType() == ManipulationType.CREATE) {
-					Element el = m.getManipulatedGroup()
-							.getChildrenElementList().getFirst().getElement();
-					snap = Snap.getSnap(up, m.getSnapCoordinates(), el.getX(),
-							el.getY());
+					Element el = m.getManipulatedGroup().getChildrenElementList().getFirst().getElement();
+					snap = Snap.getSnap(up, m.getSnapCoordinates(), el.getX(), el.getY());
 				} else {
 					snap = Snap.getSnap(up, m.getSnapCoordinates());
 				}
-				m.replaceLastManipulationCoordinates(snap.getUnitX(), snap
-						.getUnitY());
+				m.replaceLastManipulationCoordinates(snap.getUnitX(), snap.getUnitY());
 
 				// repaint scene, it is much faster than full scene invalidate
 				Gui.getActiveScenePanel().repaint();
@@ -119,48 +110,34 @@ public class ScenePanelMouseMotionListener implements MouseMotionListener {
 		UnitPoint sb = Snap.getSnap(new UnitPoint(e.getX(), e.getY()), null);
 		Gui.getActiveScenePanel().setActualPointerCoordinates(sb);
 		// adjust coordinate according to relative start
-		sb
-				.setX(sb.getX()
-						- Gui.getActiveScenePanel().getRelativeStart().getX());
-		sb
-				.setY(sb.getY()
-						- Gui.getActiveScenePanel().getRelativeStart().getY());
+		sb.setX(sb.getX() - Gui.getActiveScenePanel().getRelativeStart().getX());
+		sb.setY(sb.getY() - Gui.getActiveScenePanel().getRelativeStart().getY());
 		// set label
 		StatusBar.getInstance().setCoordinatesJLabel(
-				"X: " + sb.getUnitX() + " Y: " + sb.getUnitY() + " "
-						+ Gui.getActiveScenePanel().getRelativeStart());
+				"X: " + sb.getUnitX() + " Y: " + sb.getUnitY() + " " + Gui.getActiveScenePanel().getRelativeStart());
 
 		try {
 			Manipulation m = Gui.getActiveScenePanel().getActiveManipulation();
 
 			// manipulation is active
 			if (m.isActive()) {
-				UnitPoint up = m.getScaledUnitPoint(e.getX(), e.getY(), Gui
-						.getActiveScenePanel().getZoomFactor());
+				UnitPoint up = m.getScaledUnitPoint(e.getX(), e.getY(), Gui.getActiveScenePanel().getZoomFactor());
 				UnitPoint snap;
 				if (m.getManipulationType() == ManipulationType.CREATE) {
-					Element el = m.getManipulatedGroup()
-							.getChildrenElementList().getFirst().getElement();
-					snap = Snap.getSnap(up, m.getSnapCoordinates(), el.getX(),
-							el.getY());
+					Element el = m.getManipulatedGroup().getChildrenElementList().getFirst().getElement();
+					snap = Snap.getSnap(up, m.getSnapCoordinates(), el.getX(), el.getY());
 
 					// get pointer rectangle
-					Rectangle2D.Double r2d = Support.createPointerRectangle(
-							new Point2D.Double(e.getX(), e.getY()),
-							GuiConfiguration.getInstance()
-									.getPointerRectangle());
+					Rectangle2D.Double r2d = Support.createPointerRectangle(new Point2D.Double(e.getX(), e.getY()),
+							GuiConfiguration.getInstance().getPointerRectangle());
 
 					System.err.println(Boolean.toString(el.getElementType() == ElementType.T_WIRE));
 					if (el.getElementType() == ElementType.T_WIRE) {
-						Point2D p1 = new Point2D.Double(el.getX().get(0).doubleValue(), el
-								.getY().get(0).doubleValue());
-						Point2D p2 = new Point2D.Double(el.getX().get(1).doubleValue(), el
-								.getY().get(1).doubleValue());
-						lejblik.setText(p1.distance(p2) + "");
-						// test, if distance between start of wire and current pointer position is greater, than 20
-						if(p1.distance(p2) > 20) {
-							if (findHit(Gui.getActiveScenePanel().getSceneGraph()
-									.getTopNode(), r2d)) {
+							
+						// test, if distance between start of wire and current
+						// pointer position is greater, than 20
+						//	if (p1.distance(p2) > 20) {
+							if (findHit(Gui.getActiveScenePanel().getSceneGraph().getTopNode(), r2d)) {
 								logger.info("YYYYYY  Vyrobeno krizeni XXXXXXXXXXXXX");
 								lejblik.setText("Vyrabim");
 								lejblik.setText("2 Vyrabim");
@@ -169,48 +146,42 @@ public class ScenePanelMouseMotionListener implements MouseMotionListener {
 
 									lejblik.setText("Za try Vyrabim");
 
-									JunctionNode point = NodeFactory
-											.createJunctionNode(new Junction());
+									JunctionNode point = NodeFactory.createJunctionNode(new Junction());
 									GroupNode mess = NodeFactory.createGroupNode();
-									ParameterNode option = NodeFactory
-											.createParameterNode();
+									ParameterNode option = NodeFactory.createParameterNode();
 
 									mess.add(option);
 									mess.add(point);
 
-									Manipulation newBorn = ManipulationFactory.create(
-											ManipulationType.CREATE, Gui
-													.getActiveScenePanel()
-													.getSceneGraph().getTopNode(),
-											null, mess);
+									Manipulation newBorn = ManipulationFactory.create(ManipulationType.CREATE, Gui
+											.getActiveScenePanel().getSceneGraph().getTopNode(), null, mess);
 									// newBorn.setManipulatedGroup(mess);
-									newBorn.addManipulationCoordinates(
-											new Pixel(r2d.x), new Pixel(r2d.y));
+									newBorn.addManipulationCoordinates(new Pixel(r2d.x), new Pixel(r2d.y));
 
-									Gui.getActiveScenePanel().getManipulationQueue()
-											.execute(newBorn);
-									Gui.getActiveScenePanel().sceneInvalidate(null);
+									if(Gui.isDoAfterActive()){
+										lejblik.setText("Máme pøipraveno");
+										Gui.setDoAfter(newBorn);
+									} else lejblik.setText("Nemáme pøipraveno");
+										
+
 
 									lejblik.setText("Vyrabim: done");
 
-									logger
-											.info("XXXXXXXXXX  Vyrobeno krizeni XXXXXXXXXXXXX");
+									logger.info("XXXXXXXXXX  Vyrobeno krizeni XXXXXXXXXXXXX");
 
 								} catch (UnknownManipulationException h) {
-									logger.error("Error making Junction "
-											+ h.toString());
+									logger.error("Error making Junction " + h.toString());
 								} catch (UnknownError f) {
 									logger.error("Neznámá chyba");
 								}
 
 							}
-						}
+						//}
 					}
 				} else {
 					snap = Snap.getSnap(up, m.getSnapCoordinates());
 				}
-				m.replaceLastManipulationCoordinates(snap.getUnitX(), snap
-						.getUnitY());
+				m.replaceLastManipulationCoordinates(snap.getUnitX(), snap.getUnitY());
 
 				// repaint scene, it is much faster than full scene invalidate
 				Gui.getActiveScenePanel().repaint();
@@ -232,14 +203,12 @@ public class ScenePanelMouseMotionListener implements MouseMotionListener {
 		boolean spojeni = false;
 
 		int i = 0;
-		this.lejblik.setText(lejblik.getText() + "\n prvku "
-				+ mapleLeaves.size());
+		this.lejblik.setText(lejblik.getText() + "\n prvku " + mapleLeaves.size());
 
 		for (i = 0; i < mapleLeaves.size(); i++) {
 			// let me know that you're going through the list
 
-			spojeni = mapleLeaves.get(i).isHit(r2d,
-					Gui.getActiveScenePanel().getZoomFactor(), null);
+			spojeni = mapleLeaves.get(i).isHit(r2d, Gui.getActiveScenePanel().getZoomFactor(), null);
 
 			// did I hit against the wire
 			if (mapleLeaves.get(i).getElementType() == ElementType.T_WIRE)
